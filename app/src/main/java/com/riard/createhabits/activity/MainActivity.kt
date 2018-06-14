bd4fc1e6c91b8d0ce9f57897241726767808ac31
bilousov_alex <alexey.bilousov.1995@gmail.com>
2018-06-14 17:24:38 +0300

    Initial commit

---
+++ app/src/main/java/com/riard/createhabits/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.riard.createhabits.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.riard.createhabits.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
